a121481c1c58441b1ad9ac4f7959f9940a94228c
matimesch <matimesch@gmail.com>
2023-05-23 15:40:52 -0300

    1.1 fix route paths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "580709dc1455d133"; }
+/******/ 		__webpack_require__.h = function() { return "a6db5ab41600faab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.fcca90612ebd35c7.hot-update.js
+++ .next/static/webpack/webpack.fcca90612ebd35c7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "06a4f291df903c5a"; }
+/******/ 	__webpack_require__.h = function() { return "a6db5ab41600faab"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
