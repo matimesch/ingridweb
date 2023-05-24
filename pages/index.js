1235e3dca62065dc932df4cc7456f88aa4e24e4b
matimesch <matimesch@gmail.com>
2023-05-24 13:19:08 -0300

    1.3 Agrego contratapa y corrijo email

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Layout from "../components/Layout";
 
 const myStyle = {
-  backgroundImage: "url('/images/tapa.jpg')",
+  backgroundImage: "url('/images/tapa3.jpg')",
+  height: "100vh",
+  fontSize: "50px",
+  backgroundSize: "100% 100%",
+  backgroundRepeat: "no-repeat",
+};
+const myStyle2 = {
+  backgroundImage: "url('/images/parte_atras.jpg')",
   height: "100vh",
   fontSize: "50px",
   backgroundSize: "100% 100%",
@@ -14,13 +21,18 @@ const Index = () => (
       <div className="container">
         <div className="row">
           <div className="col">
-            <p>Contáctate conmigo en:</p>
-            <p>ingridbossero@hotmail.com</p>
-            <span>inbossero@gmail.com</span>
+            <div className="card">
+              <div className="card-body">
+                <p className="text-white">Contáctate conmigo en:</p>
+                <p className="text-white">ingridbossero@hotmail.com</p>
+                <span className="text-white">ingridvbossero@gmail.com</span>.
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div style={myStyle}></div>
+      <div style={myStyle2}></div>
     </>
   </Layout>
 );
